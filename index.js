dba33b12b2373b45c95dc2708e32a42dac3938f8
Asad Hussain <AsadHusn007@gmail.com>
2024-10-16 18:48:36 +0000

    solve

--- index.js
+++ index.js
@@ -1,24 +1,53 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
+const dns = require("dns");
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+let count = 0;
+const urls = {};
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+app.post("/api/shorturl", function (req, res) {
+  const url = req.body.url;
+  if (!url) return res.json({ error: "invalid url" });
+  dns.lookup(url, (err, address, family) => {
+    if (err) {
+      console.error("Domain does not resolve:", err);
+      return res.json({ error: "invalid url" });
+    } else {
+      console.log("IP address:", address); // e.g., 93.184.216.34
+      console.log("IP version:", family); // e.g., IPv4
+      count++;
+      const shorturl = {
+        original_url: url,
+        short_url: count,
+      };
+      urls[count] = shorturl;
+
+      return res.json(shorturl);
+    }
+  });
+  // res.json({ original_url: "https://freeCodeCamp.org", short_url: 1 });
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
