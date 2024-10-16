7a9c8e960f1cfa1e14aa695971e5adb02161cfa4
Asad Hussain <AsadHusn007@gmail.com>
2024-10-16 19:50:44 +0000

    solve problems

--- index.js
+++ index.js
@@ -25,28 +25,42 @@ app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
 });
 
-app.post("/api/shorturl", function (req, res) {
-  const url = req.body.url;
-  if (!url) return res.json({ error: "invalid url" });
-  dns.lookup(url, (err, address, family) => {
-    if (err) {
-      console.error("Domain does not resolve:", err);
-      return res.json({ error: "invalid url" });
-    } else {
-      console.log("IP address:", address); // e.g., 93.184.216.34
-      console.log("IP version:", family); // e.g., IPv4
-      count++;
-      const shorturl = {
-        original_url: url,
-        short_url: count,
-      };
-      urls[count] = shorturl;
+app
+  .route("/api/shorturl/:shorturl?")
+  .post(function (req, res) {
+    if (!req.body.url) throw new Error();
+    try {
+      const url = new URL(req.body.url);
+      console.log("url", url);
+      dns.lookup(url.hostname, (err, address, family) => {
+        if (err) {
+          console.error("Domain does not resolve:", err);
+          return res.json({ error: "invalid url" });
+        } else {
+          console.log("IP address:", address); // e.g., 93.184.216.34
+          console.log("IP version:", family); // e.g., IPv4
+          count++;
+          const shorturl = {
+            original_url: url,
+            short_url: count,
+          };
+          urls[count] = shorturl;
 
-      return res.json(shorturl);
+          return res.json(shorturl);
+        }
+      });
+    } catch (error) {
+      return res.json({ error: "Invalid URL" });
     }
+  })
+  .get((req, res) => {
+    console.log("IN GET");
+    console.log("param", req.params);
+    const url = urls[req.params.shorturl];
+    return url
+      ? res.redirect(url.original_url)
+      : res.status(404).send("Not found");
   });
-  // res.json({ original_url: "https://freeCodeCamp.org", short_url: 1 });
-});
 
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
